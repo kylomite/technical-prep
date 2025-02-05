2f9d0ae94dd11b3ccf8f753ef50f6331517be5bd
kylomite <kdelaney99@gmail.com>
2025-02-05 11:17:45 -0500

    add condensed solution

--- solutions/reverseInt.js
+++ solutions/reverseInt.js
@@ -1,27 +1,40 @@
+// export default function reverseInteger(input) {
+//   let inputArray = String(Math.abs(input)).split('').map(Number);
+//   let result = ""
+
+//   inputArray.forEach((num) => {
+//     result = `${num}${result}`;
+//   });
+
+//   if(Math.sign(input) === -1) {
+//     result = `-${result}`
+//     return parseInt(result);
+//   }
+//   else if(result > 7463847411) {
+//     return 0
+//   }
+//   else {
+//     return parseInt(result);
+//   }
+// }
+
+// console.log(reverseInteger(1234))
+
+// console.log(reverseInteger(-1234))
+
+// console.log(reverseInteger(120))
+
+// console.log(reverseInteger(2147483647))
+
 export default function reverseInteger(input) {
-  let inputArray = String(Math.abs(input)).split('').map(Number);
-  let result = ""
+  const limit = Math.pow(2, 31);
+  const isNegative = input < 0;
+  
+  let reversed = parseInt(
+    Math.abs(input).toString().split('').reverse().join('')
+  );
 
-  inputArray.forEach((num) => {
-    result = `${num}${result}`;
-  });
+  if (reversed >= limit) return 0;
 
-  if(Math.sign(input) === -1) {
-    result = `-${result}`
-    return parseInt(result);
-  }
-  else if(result > 7463847411) {
-    return 0
-  }
-  else {
-    return parseInt(result);
-  }
+  return isNegative ? -reversed : reversed;
 }
-
-console.log(reverseInteger(1234))
-
-console.log(reverseInteger(-1234))
-
-console.log(reverseInteger(120))
-
-console.log(reverseInteger(2147483647))
